b57d954dd599714ac8f10845637cd2988bd78401
vendetta-6 <79677991+vendetta-6@users.noreply.github.com>
2025-04-02 01:13:06 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('robot-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const answer = document.getElementById('answer').value;
-    if (parseInt(answer) === 19) {
-        window.location.href = 'final.html';
-    } else {
-        alert('Incorrect answer. Please try again.');
-    }
-});
